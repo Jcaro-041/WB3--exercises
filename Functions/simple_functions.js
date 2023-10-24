c903395fb7c5891ae288dd674744c9baece555c9
Jonathan Caro <jcaro4001@gmail.com>
2023-10-24 13:32:13 -0700

    Created new files and folder

---
+++ Functions/simple_functions.js
@@ -0,0 +1,18 @@
+function favoriteThings(){
+    console.log("My name is Jonathan Caro");
+    console.log("One of my favorite movies recently was Spider-Man Across the Spider-Verse");
+    console.log("One of my favorite bands is Red Hot Chili Peppers");
+}
+function whyImHere(){
+    console.log("I joined this 'LTC' program because I knew it would help me to understand coding better and also network with others who are in the same boat as me");
+}
+function favoritePlace(){
+    console.log("One of my favorite places to visit is Orange, California because that's where I have family and everytime I'm there I don't stress about school or work I just hang out and chill with my family");
+}
+favoriteThings();
+console.log("------------------------------");
+whyImHere();
+console.log("------------------------------");
+favoritePlace();
+console.log("------------------------------")
+console.log("end of file");
